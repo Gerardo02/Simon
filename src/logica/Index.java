a759da3526efd51c0cc3a8157047660c9b45d465
Gerardo02 <54118434+Gerardo02@users.noreply.github.com>
2023-11-22 14:47:37 -0700

    first

---
+++ src/logica/Index.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+
+
+import pantallas.Main;
+
+/**
+ *
+ * @author gerar
+ */
+public class Index {
+    public static void main(String[] args) {
+        Main mainFrame = new Main();
+        mainFrame.setVisible(true);
+        
+        
+  
+    }
+}
